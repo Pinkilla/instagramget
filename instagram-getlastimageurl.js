6dddb97b65e4aa5e8574775d84340a4a92bcedf1
Pierre Bettens (pbt) <pbettens@heb.be>
2015-07-16 11:54:52 +0200

    Ajout du script phantomjs
    Script permettant de récupérer l'url de la dernière image postée sur
    instagram

---
+++ instagram-getlastimageurl.js
@@ -0,0 +1,25 @@
+var page = require('webpage').create(),
+	system = require('system'),
+	account = 'default',
+	url,
+	image_url
+
+if (system.args.length === 2){
+	account = system.args[1]
+}
+url = 'instagram.com/' + account
+
+page.open('http://' + url, function(status) {
+	if (status === 'success'){
+		image_url = page.evaluate(function() {
+			return window.pImage_0.src;
+			//return document.title
+		})
+		console.log(image_url)
+	} else {
+		console.log('Fail to open ' + url)
+	}
+	phantom.exit()
+})
+
+
