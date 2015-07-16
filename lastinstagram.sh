a1183fe54104ce1294d3c02aa32c84a3a78d0efe
Pinkilla <pb@namok.be>
2015-07-16 15:57:30 +0200

    Ajout de l'envoi d'un mail

--- lastinstagram.sh
+++ lastinstagram.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER=mazaurette
+MAIL=pb@namok.be
 
 # Init. current and previous file must
 if [ ! -f current ]; then 
@@ -13,10 +14,15 @@ fi
 
 /usr/bin/diff current previous > /dev/null
 if [ $? -eq 1 ]; then
-	echo "différents"
 	/usr/bin/phantomjs --ssl-protocol=any instagram-getlastimageurl.js $USER \
 		> current
 	/usr/bin/wget --directory-prefix img $(/bin/cat current)
 	/bin/cp current previous
+	
+	# Send by mail
+    NAME=$(/bin/cat current)
+    NAME=${NAME##*/}
+    echo "Plop" | /usr/bin/mail -s "Instagram from Maïa" -A img/$NAME $MAIL
+
 fi
 
