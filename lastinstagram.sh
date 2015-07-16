6304d646fb3eb3c86a880442a3b882c8c5162c5e
Pierre Bettens (pbt) <pbettens@heb.be>
2015-07-16 12:01:14 +0200

    Ajout current et previous
    Le script vérifie l'existence des fichiers current et previous. Il les crée
    si nécessaire

--- lastinstagram.sh
+++ lastinstagram.sh
@@ -3,13 +3,20 @@
 USER=mazaurette
 
 # Init. current and previous file must
+if [ ! -f current ]; then 
+	echo 0 > current
+fi
+
+if [ ! -f previous ]; then
+	echo 1 > previous
+fi
 
 /usr/bin/diff current previous > /dev/null
 if [ $? -eq 1 ]; then
 	echo "différents"
-	/bin/cp current previous
 	/usr/bin/phantomjs --ssl-protocol=any instagram-getlastimageurl.js $USER \
 		> current
 	/usr/bin/wget --directory-prefix img $(/bin/cat current)
+	/bin/cp current previous
 fi
 
